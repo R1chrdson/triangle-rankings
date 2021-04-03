e8f987fd8755fd17a9aefba47d90b1c0e86428ee
R1chrdson <prostoandrey01@gmail.com>
2021-04-04 01:34:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QSizePolicy, QHeaderView, QTableView
+from PyQt5.QtGui import QIntValidator, QRegExpValidator
+from PyQt5.QtCore import QRegExp, QAbstractTableModel, Qt, QVariant
+from triangle import triangular, metric
+import numpy as np
+
+
+class PandasModel(QAbstractTableModel):
+    def __init__(self, data):
+        QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return len(self._data.values)
+
+    def columnCount(self, parent=None):
+        return self._data.columns.size
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return QVariant(str(self._data.iloc[index.row()][index.column()]))
+        return QVariant()
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return str(self._data.columns[col])
+
+        if orientation == Qt.Vertical and role == Qt.DisplayRole:
+            return str(self._data.index[col])
+        return None
+
+
+def get_float(string):
+    if not string:
+        raise ValueError('Empty value!')
+    return float(string.replace(',', '.'))
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('ui/ui/MainPage.ui', self)
+        # self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.generate_button.clicked.connect(self.generate)
+
+        edits = [self.a1_edit, self.b1_edit, self.m1_edit,
+                 self.a2_edit, self.b2_edit, self.m2_edit]
+
+        self.prev = np.array([float(edit.text()) for edit in edits])
+        for edit in edits:
+            edit.setValidator(QRegExpValidator(QRegExp(r'-?[0-9]+[.|,][0-9]+')))
+
+        self.size_edit.setValidator(QIntValidator(2, 1000000))
+
+    def generate(self):
+        try:
+            a1, b1, m1 = get_float(self.a1_edit.text()), get_float(self.b1_edit.text()), get_float(self.m1_edit.text())
+            a2, b2, m2 = get_float(self.a2_edit.text()), get_float(self.b2_edit.text()), get_float(self.m2_edit.text())
+            size = int(self.size_edit.text())
+            r1 = triangular(a1, b1, m1, size)
+            r2 = triangular(a2, b2, m2, size)
+            current = np.array([a1, b1, m1, a2, b2, m2])
+
+            df, metric_value, metric_rank = metric(r1, r2)
+
+            model = PandasModel(df)
+            self.table.setModel(model)
+            non_zero_prev = self.prev != 0
+            print(non_zero_prev)
+            if (((current[non_zero_prev] / self.prev[non_zero_prev]) >= 10).any() or
+                ((current[non_zero_prev] / self.prev[non_zero_prev]) <= 0.1).any() or
+                self.flag):
+                self.table.resizeColumnsToContents()
+            self.prev = current
+
+            self.metric_value_result.setText(str(metric_value))
+            self.metric_rank_result.setText(str(metric_rank))
+        except Exception as e:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(type(e).__name__)
+            msg.setInformativeText(str(e))
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.setWindowTitle("Ranking Generator")
+    window.setFixedSize(767, 655)
+    window.show()
+    sys.exit(app.exec_())
---
+++ triangle.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+
+
+def rnd():
+    return np.random.random()
+
+
+def get_left_triangle(a, m):
+    return a + (m - a) * np.sqrt(rnd())
+
+
+def get_right_triangle(b, m):
+    return m + (b - m) * (1 - np.sqrt(rnd()))
+
+
+def triangular(a, b, m, size):
+    if a > b:
+        raise ValueError('a > b')
+    if m < a:
+        raise ValueError('m < a')
+    if m > b:
+        raise ValueError('m > b')
+    if size <= 0:
+        raise ValueError('size <= 0')
+
+    if a == b:
+        return np.array([np.float64(a) for _ in range(size)])
+
+    p1 = (m - a) / (b - a)
+    return np.array([get_left_triangle(a, m) if rnd() < p1 else get_right_triangle(b, m) for _ in range(size)])
+
+
+def metric(r1, r2):
+    if sum(r1) == 0:
+        raise ValueError('Sum of R1 is zero.\nCan\'t be normed')
+    if sum(r2) == 0:
+        raise ValueError('Sum of R2 is zero.\nCan\'t be normed')
+
+    r1_normed = r1 / sum(r1)
+    r2_normed = r2 / sum(r2)
+    diff = np.absolute(r1_normed - r2_normed)
+    metric = diff.sum()
+
+    r1_ranks = np.argsort(r1.argsort()[::-1])
+    r2_ranks = np.argsort(r2.argsort()[::-1])
+    diff_ranks = np.absolute(r1_ranks - r2_ranks)
+    metric_ranks = diff_ranks.sum()
+
+    data = {'R1': r1, 'R2': r2, 'R1_n': r1_normed, 'R2_n': r2_normed, 'D_n': diff,
+            'R1_r': r1_ranks, 'R2_r': r2_ranks, 'D_r': diff_ranks}
+    df = pd.DataFrame(data=data)
+    df['R1'] = df['R1'].map(lambda x: '{0:5.2f}'.format(x))
+    df['R2'] = df['R2'].map(lambda x: '{0:5.2f}'.format(x))
+    df['R1_n'] = df['R1_n'].map(lambda x: '{0:5.4f}'.format(x))
+    df['R2_n'] = df['R2_n'].map(lambda x: '{0:5.4f}'.format(x))
+    df['D_n'] = df['D_n'].map(lambda x: '{0:5.4f}'.format(x))
+    metric_str = '{0:5.4f}'.format(metric)
+    return df, metric_str, metric_ranks
